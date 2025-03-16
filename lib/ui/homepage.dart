ac5955e9b34f52bbd9517c3d2058768f8644f8d8
Saheed Ansari <shahidansari531@gmail.com>
2025-03-16 15:14:23 +0530

    new file added

---
+++ lib/ui/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: Text('Homepage'),
+        ),
+      ),
+    );
+  }
+}
